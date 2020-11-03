bac633ea191f35dd4c982700a5ae6b198be7f228
Bruno Ardel <djbrunohardel@hotmail.com>
2020-11-03 18:06:37 -0300

    Escrevendo o primeiro teste

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Logo from '.'
 
@@ -9,5 +10,12 @@ describe('<Logo />', () => {
     // expect - assertion - asserção - comparação - análise (espero que renderize a logo branca)
     //
     
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({color: '#FAFAFA'})
+  })
+
+  it('should render a black label when color is passed', () => {
+    renderWithTheme(<Logo color="black"/>)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({color: '#030517'})
   })
 })
