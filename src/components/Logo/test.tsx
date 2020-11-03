f22cd95575390dfcd7ef5a212b95e0936c67bd6c
Bruno Ardel <djbrunohardel@hotmail.com>
2020-11-03 18:21:24 -0300

    Escrevendo teste de cor e tamanho

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -3,13 +3,13 @@ import { renderWithTheme } from 'utils/tests/helpers'
 
 import Logo from '.'
 
+// renderizar o componente "render"
+// selecionar o elemento a ser testado "screen" (queries) - getByLabel ...
+// expect - assertion - asserção - comparação - análise (espero que renderize a logo branca)
+//
+
 describe('<Logo />', () => {
-  it('should render a white label by default', () => {
-    // renderizar o componente "render"
-    // selecionar o elemento a ser testado "screen" (queries) - getByLabel ...
-    // expect - assertion - asserção - comparação - análise (espero que renderize a logo branca)
-    //
-    
+  it('should render a white label by default', () => {    
     renderWithTheme(<Logo />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({color: '#FAFAFA'})
   })
@@ -17,5 +17,15 @@ describe('<Logo />', () => {
   it('should render a black label when color is passed', () => {
     renderWithTheme(<Logo color="black"/>)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({color: '#030517'})
+  })
+
+  it('should render a normal logo when size is default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({width: '11rem'})
+  })
+
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large"/>)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({width: '20rem'})
   })
 })
