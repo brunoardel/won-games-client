f834c872f32672aa2bb2deb6bb04e4f3be77c9ce
Bruno Ardel <djbrunohardel@hotmail.com>
2020-11-04 04:14:25 -0300

    Adicionando icone e atualizando o teste

--- readme.md
+++
@@ -1,1 +0,0 @@
-# won-games-client
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,17 +1,22 @@
 import * as S from './styles'
 
 export type ButtonProps = {
+  icon?: JSX.Element
   children?: React.ReactNode
   size?: 'small' | 'medium' | 'large'
   fullWidth?: boolean
+  onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void
 }
 
 const Button = ({
+  icon,
   children,
   size = 'medium',
-  fullWidth = false
+  fullWidth = false,
+  ...props
 }: ButtonProps) => (
-  <S.Wrapper size={size} fullWidth={fullWidth}>
+  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon} {...props}>
+    {icon}
     {!!children && <span>{children}</span>}
   </S.Wrapper>
 )
