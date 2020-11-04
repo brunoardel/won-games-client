5e28ff96879681436598a0a35178e446e1f8bf14
Bruno Ardel <djbrunohardel@hotmail.com>
2020-11-04 17:21:08 -0300

    Melhorando acessibilidade e adicionando testes dos icones

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -6,5 +6,10 @@ import Menu from '.'
 describe('<Menu />', () => {
   it('should render menu', () => {
     renderWithTheme(<Menu />)
+
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
   })
 })
