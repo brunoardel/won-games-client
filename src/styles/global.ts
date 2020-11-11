23cd9c701b015300f21318a1defa4ab489f509da
Bruno Ardel <djbrunohardel@hotmail.com>
2020-11-11 00:52:16 -0300

    Ajustes nos estilos globais e removendo background global do Storybook

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,18 @@
-import { createGlobalStyle, css } from 'styled-components'
+import {
+  createGlobalStyle,
+  css,
+  DefaultTheme,
+  GlobalStyleComponent
+} from 'styled-components'
 
-const GlobalStyles = createGlobalStyle`
+type GlobalStylesProps = {
+  removeBg?: boolean
+}
+
+const GlobalStyles: GlobalStyleComponent<
+  GlobalStylesProps,
+  DefaultTheme
+> = createGlobalStyle`
   @font-face {
     font-family: 'Poppins';
     font-style: normal;
@@ -9,6 +21,7 @@ const GlobalStyles = createGlobalStyle`
     src: local('Poppins Light'), local('Poppins-Light'),
         url('/fonts/poppins-v13-latin-300.woff2') format('woff2');
   }
+
   @font-face {
     font-family: 'Poppins';
     font-style: normal;
@@ -17,6 +30,7 @@ const GlobalStyles = createGlobalStyle`
     src: local('Poppins Regular'), local('Poppins-Regular'),
         url('/fonts/poppins-v13-latin-regular.woff2') format('woff2');
   }
+
   @font-face {
     font-family: 'Poppins';
     font-style: normal;
@@ -25,20 +39,33 @@ const GlobalStyles = createGlobalStyle`
     src: local('Poppins SemiBold'), local('Poppins-SemiBold'),
         url('/fonts/poppins-v13-latin-600.woff2') format('woff2');
   }
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+
+    &::before,
+    &::after {
+      box-sizing: inherit;
+    }
   }
-  ${({ theme }) => css`
+
+  ${({ theme, removeBg }) => css`
     html {
       font-size: 62.5%;
     }
+
     body {
       font-family: ${theme.font.family};
       font-size: ${theme.font.sizes.medium};
+
+      ${!removeBg &&
+      css`
+        background-color: ${theme.colors.mainBg};
+      `}
     }
   `}
 `
