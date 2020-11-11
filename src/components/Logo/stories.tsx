b5cc65a1e6deb881dbb5cedab9c1b53fddd99820
Bruno Ardel <djbrunohardel@hotmail.com>
2020-11-11 01:21:29 -0300

    Criando backgrounds customizados para o Storybook

--- src/components/Logo/stories.tsx
+++ src/components/Logo/stories.tsx
@@ -1,9 +1,12 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import Logo, {LogoProps} from '.'
+import Logo, { LogoProps } from '.'
 
 export default {
   title: 'Logo',
-  component: Logo
+  component: Logo,
+  backgrounds: {
+    default: 'won-dark'
+  }
 } as Meta
 
 export const Default: Story<LogoProps> = (args) => <Logo {...args} />
