c36b089ee9bb1b8968499f6f789cb46d22e4eaf7
Bruno Ardel <djbrunohardel@hotmail.com>
2020-11-12 23:40:55 -0300

    Button - criando variavel minimal

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,20 +6,28 @@ type ButtonTypes =
   | ButtonHTMLAttributes<HTMLButtonElement>
 
 export type ButtonProps = {
-  icon?: JSX.Element
   size?: 'small' | 'medium' | 'large'
   fullWidth?: boolean
+  minimal?: boolean
+  icon?: JSX.Element
   as?: React.ElementType
 } & ButtonTypes
 
 const Button = ({
-  icon,
   children,
+  icon,
   size = 'medium',
   fullWidth = false,
+  minimal = false,
   ...props
 }: ButtonProps) => (
-  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon} {...props}>
+  <S.Wrapper
+    size={size}
+    fullWidth={fullWidth}
+    hasIcon={!!icon}
+    minimal={minimal}
+    {...props}
+  >
     {icon}
     {!!children && <span>{children}</span>}
   </S.Wrapper>
